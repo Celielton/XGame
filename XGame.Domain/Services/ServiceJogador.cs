5e7ac480d5ab45eaaa590fadbe9c9c80a8fe6a82
Celielton Ramalho <celton1712@hotmail.com>
2018-05-05 22:14:38 -0300

    Aula 5 - Repository

--- XGame.Domain/Services/ServiceJogador.cs
+++ XGame.Domain/Services/ServiceJogador.cs
@@ -1,19 +1,28 @@
 ﻿using System;
 using XGame.Domain.Arguments.Jogador;
+using XGame.Domain.Interfaces.Repositories;
 using XGame.Domain.Interfaces.Services;
 
 namespace XGame.Domain.Services
 {
     public class ServiceJogador : IServiceJogador
     {
+        private readonly IRepositoryJogador _repository;
+
+        public ServiceJogador(IRepositoryJogador repository)
+        {
+            _repository = repository;
+        }
+
         public AdicionarJogadorResponse AdicionarJogador(AdicionarJogadorRequest request)
         {
-            throw new NotImplementedException();
+           Guid id =  _repository.AdicionarJogador(request);
+            return new AdicionarJogadorResponse() { Id = id, Message = "Operação realizada com sucesso!" };
         }
 
         public AutenticarJogadorResponse AutenticarJogador(AutenticarJogadorRequest request)
         {
-            throw new NotImplementedException();
+            return _repository.AutenticarJogador(request);
         }
     }
 }
